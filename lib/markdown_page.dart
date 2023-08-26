d9c76f93ff868c01bc0a9abaa0d04b5b290928ef
陈浩 <32390273+chenhaoaixuexi@users.noreply.github.com>
2023-08-26 12:36:22 +0800

    [feat] stateless=> state ful

--- lib/markdown_page.dart
+++ lib/markdown_page.dart
@@ -1,7 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_pasteboard/ClipboardVM.dart';
-import 'package:flutter_pasteboard/utils/SimpleGetController.dart';
-import 'package:flutter_pasteboard/utils/function.dart';
 import 'package:get/get.dart';
 
 /**
@@ -9,60 +9,77 @@ import 'package:get/get.dart';
  * @author chenhao91
  * @date   2023/8/22
  */
-class MarkdownPage extends StatelessWidget {
-  MarkdownPage({Key? key}) : super(key: key);
+class MarkdownPage extends StatefulWidget {
+  const MarkdownPage({Key? key}) : super(key: key);
+
+  MarkdownPageState createState() => MarkdownPageState();
+}
+
+class MarkdownPageState extends State<MarkdownPage> {
   final ClipboardVM clipboardVM = Get.find<ClipboardVM>();
+
+  late TextEditingController textController;
+  late StreamSubscription sub;
+
+  @override
+  void initState() {
+    super.initState();
+    textController =
+        TextEditingController(text: clipboardVM.editMarkdownContext);
+    sub = clipboardVM.lastClipTxt.listen((p0) {
+      var origin = textController.text;
+      var selection = textController.selection;
+      var insertPos = selection.extentOffset;
+      if (insertPos <= 0 || insertPos >= origin.length) {
+        insertPos = origin.length;
+        p0 = "\n$p0";
+      }
+      textController.text =
+          "${origin.substring(0, insertPos)}$p0${origin.substring(insertPos)}";
+      print("controller.text = ${textController.text}");
+      textController.selection = TextSelection.fromPosition(
+          TextPosition(offset: insertPos + p0.length));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    sub.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
-    var controller =
-        TextEditingController(text: clipboardVM.editMarkdownContext);
     var textField = TextField(
       autofocus: true,
-      controller: controller,
-      decoration: InputDecoration(border: InputBorder.none,
+      controller: textController,
+      decoration: InputDecoration(
+          border: InputBorder.none,
           hintText: "The content you copied will automatically come here. "
               "\n😘Try cmd c to copy something",
           hintStyle: TextStyle(fontSize: 14, color: Colors.grey.shade400),
-          contentPadding: const EdgeInsets.only(left: 20, right: 20, top: 8, bottom: 8)),
+          contentPadding:
+              const EdgeInsets.only(left: 20, right: 20, top: 8, bottom: 8)),
       maxLines: null,
       style: const TextStyle(fontSize: 14),
       onChanged: (value) {
         clipboardVM.editMarkdownContext = value;
       },
     );
-    var core = GetBuilder(
-      init: SimpleGetController(onReadyFunc: () {
-        return clipboardVM.lastClipTxt.listen((p0) {
-          var origin=controller.text;
-          var selection = controller.selection;
-          var insertPos = selection.extentOffset;
-          if (insertPos <= 0 || insertPos >= origin.length) {
-            insertPos = origin.length;
-            p0 = "\n$p0";
-          }
-          controller.text =
-              "${origin.substring(0, insertPos)}$p0${origin.substring(insertPos)}";
-          print("controller.text = ${controller.text}");
-          controller.selection = TextSelection.fromPosition(
-              TextPosition(offset: insertPos + p0.length));
-        });
-      }),
-      builder: (sc) {
-        return textField;
-      },
-    );
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 30,
         leading: IconButton(
-            onPressed: () {
-              Get.back();
-            },
+          onPressed: () {
+            Get.back();
+          },
           icon: const Icon(Icons.arrow_back_ios_sharp),
           iconSize: 16,
         ),
-        title: const Text("Markdown Mode", style: TextStyle(fontSize: 15),),
+        title: const Text(
+          "Markdown Mode",
+          style: TextStyle(fontSize: 15),
+        ),
         actions: [
           IconButton(onPressed: () {
             clipboardVM.alwaysOnTop.value = !clipboardVM.alwaysOnTop.value;
@@ -73,7 +90,7 @@ class MarkdownPage extends StatelessWidget {
           })),
         ],
       ),
-      body: core,
+      body: textField,
     );
   }
 }
